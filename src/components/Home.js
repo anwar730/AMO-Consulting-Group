f26913df1aff3e28ad9a92eef054636dc73a586d
anwarismail <anwaarismail449@gmail.com>
2024-07-21 23:53:39 +0300

    Add and style choose us section and make it responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,67 @@
 import React from "react";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { GrPersonalComputer } from "react-icons/gr";
+import { IoBookSharp } from "react-icons/io5";
+import { LiaCertificateSolid } from "react-icons/lia";
 function Home() {
   return (
-    <div className="hero-section">
-    <div>
-    <h1>Welcome to <span>PowerPro Academy</span></h1> 
-    <h4>Empower Your Skills with Our Comprehensive Courses</h4>
-    <p className="welcome-msg">PowerPro Academy offer a diverse range of courses designed to enhance <br/>your skills and boost your career. Our expert instructors are dedicated <br/>to providing high-quality education that is accessible and practical.<br/> Whether you're looking to improve your professional skills or explore new interests,<br/> we have the right course for <span>you.</span>
-</p>
-<a className="start-btn">Get Started</a>
+    <>
+    
+    <section className="hero-section">
+      <div>
+        <h1>
+          Welcome to <span>PowerPro Academy</span>
+        </h1>
+        <h4>Empower Your Skills with Our Comprehensive Courses</h4>
+        <p className="welcome-msg">
+          PowerPro Academy offer a diverse range of courses designed to enhance{" "}
+          <br />
+          your skills and boost your career. Our expert instructors are
+          dedicated <br />
+          to providing high-quality education that is accessible and practical.
+          <br /> Whether you're looking to improve your professional skills or
+          explore new interests,
+          <br /> we have the right course for <span>you.</span>
+        </p>
+        <a className="start-btn">Get Started</a>
+      </div>
+      <div>
+        <img src="https://empyreanit.com.au/wp-content/uploads/2023/11/Power-platform.png" />
+      </div>
+    </section>
+    <section className="choose-us-section">
+      <h1>Why Choose Us?</h1>
+      <div className="choose-us-container">
+      <div className="choose-us-item">
+      <FaChalkboardTeacher className="icons"/>
+      <h3>Expert Instructors<br/>
+      Get personalized guidance and support throughout your learning journey.
+</h3>
+      </div>
+      <div className="choose-us-item">
+      <GrPersonalComputer className="icons"/>
+      <h3>Flexible Learning:<br/>
+      Fit learning into your busy schedule with flexible deadlines.
+</h3>
+      </div>
+      <div  className="choose-us-item">
+        <IoBookSharp className="icons"/>
+        <h3>Comprehensive Resources<br/>
+        Enjoy interactive tutorials, hands-on exercises, and real-world examples.</h3>
+      </div>
+      <div  className="choose-us-item">
+      <LiaCertificateSolid className="icons"/>
+      <h3>Certification:<br/>
+Earn certificates to showcase your new skills and enhance your resume.</h3>
+      <h3>
 
-    </div>
-    <div>
-      <img src="https://empyreanit.com.au/wp-content/uploads/2023/11/Power-platform.png"/>
-    </div>
-    </div>
+      </h3>
+      </div>
+      </div>
+      
+      
+    </section>
+    </>
   );
 }
 
